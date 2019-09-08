8c7c1811a880710415e796576f951dd61a334ce4
YukimasaFunaoka <yukimasafunaoka@gmail.com>
2019-09-09 00:04:19 +0900

    temporary checkpoint

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   page.setViewport({width: 1024, height: 800})
   const url = 'https://m98.be/guess-mixed-color/'
-  await page.goto(url, { waitUntil: 'networldidle2' });
+  await page.goto(url, { waitUntil: 'networkidle2' });
   await page.screenshot({path: 'capture.png', fullPage:false});
   await browser.close()
 })();
